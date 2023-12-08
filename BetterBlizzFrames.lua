15c63231de3467f3e1c70f74ed78a280d961627c
Body <bodifyttv@gmail.com>
2023-12-08 11:50:05 +0100

    ver update

--- BetterBlizzFrames.lua
+++ BetterBlizzFrames.lua
@@ -6,8 +6,8 @@ BBF = BBF or {}
 -- Things are getting more messy need a lot of cleaning lol
 
 local addonVersion = "1.00" --too afraid to to touch for now
-local addonUpdates = "1.1.2"
-local sendUpdate = true
+local addonUpdates = "1.1.2b"
+local sendUpdate = false
 BBF.VersionNumber = addonUpdates
 BBF.variablesLoaded = false
 
