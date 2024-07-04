b01031a5218b2d735c33eaeddc48f3c85190d45d
Body <bodifyttv@gmail.com>
2024-07-04 12:56:02 +0200

    update v number

--- temp_cata/gui.lua
+++ temp_cata/gui.lua
@@ -1568,7 +1568,7 @@ local function CreateTitle(parent)
     addonNameIcon:SetPoint("LEFT", addonNameText, "RIGHT", -2, -1)
     local verNumber = parent:CreateFontString(nil, "OVERLAY", "GameFontNormal")
     verNumber:SetPoint("LEFT", addonNameText, "RIGHT", 25, 0)
-    verNumber:SetText("CATA BETA v0.1.1" )--.. BBF.VersionNumber.."b")
+    verNumber:SetText("CATA BETA v0.1.1b" )--.. BBF.VersionNumber.."b")
 end
 
 ------------------------------------------------------------
