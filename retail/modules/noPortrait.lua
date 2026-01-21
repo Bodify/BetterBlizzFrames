ef9fefd0d7a115f9fed291e19d2cf09abd7c88a6
Bodify <bodifyttv@gmail.com>
2026-01-21 12:35:31 +0100

    fix no portrait tot background size

--- retail/modules/noPortrait.lua
+++ retail/modules/noPortrait.lua
@@ -540,10 +540,15 @@ local function MakeNoPortraitMode(frame)
         totManaBar:SetFrameLevel(1)
         totFrame.Portrait:SetParent(BBF.hiddenFrame)
 
+        local hideToTMana = (frame == TargetFrame and db.hideUnitFrameTargetMana) or (frame == FocusFrame and db.hideUnitFrameFocusMana)
         totFrame.Background = totFrame.HealthBar:CreateTexture(nil, "BACKGROUND")
         totFrame.Background:SetColorTexture(0,0,0,0.45)
         totFrame.Background:SetPoint("TOPLEFT", totFrame.HealthBar, "TOPLEFT", 1, -1)
-        totFrame.Background:SetPoint("BOTTOMRIGHT", totFrame.manabar, "BOTTOMRIGHT", -1, 1)
+        if hideToTMana then
+            totFrame.Background:SetPoint("BOTTOMRIGHT", totFrame.HealthBar, "BOTTOMRIGHT", -1, 1)
+        else
+            totFrame.Background:SetPoint("BOTTOMRIGHT", totFrame.manabar, "BOTTOMRIGHT", -1, 1)
+        end
         totFrame.FrameTexture:SetSize(130, 33)
         local totTexture = (frame == TargetFrame) and targetDefaultTex or focusDefaultTex
         totFrame.FrameTexture:SetTexture(totTexture)
@@ -773,6 +778,16 @@ local function MakeNoPortraitMode(frame)
             frame.noPortraitMode.Texture:SetTexture(textureToUse)
             frame.noPortraitMode.Background:SetPoint("BOTTOMRIGHT", contentMain.ManaBar, "BOTTOMRIGHT", -10, bgYOffset)
 
+            local hideToTMana = (frame == TargetFrame and db.hideUnitFrameTargetMana) or (frame == FocusFrame and db.hideUnitFrameFocusMana)
+            if frame.totFrame and frame.totFrame.Background then
+                frame.totFrame.Background:ClearAllPoints()
+                frame.totFrame.Background:SetPoint("TOPLEFT", frame.totFrame.HealthBar, "TOPLEFT", 1, -1)
+                if hideToTMana then
+                    frame.totFrame.Background:SetPoint("BOTTOMRIGHT", frame.totFrame.HealthBar, "BOTTOMRIGHT", -1, 1)
+                else
+                    frame.totFrame.Background:SetPoint("BOTTOMRIGHT", frame.totFrame.manabar, "BOTTOMRIGHT", -1, 1)
+                end
+            end
         end)
 
         hooksecurefunc(frame, "CheckFaction", function(self)
